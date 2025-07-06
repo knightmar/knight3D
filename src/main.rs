c2521bc97fd34ab7d1a8d2e4604868eb371bcb9c
knightmar <arthur.petit.d@gmail.com>
2025-07-06 18:16:05 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-extern crate glad_gl;
-
 use glad_gl::gl;
 use glad_gl::gl::{GLsizei, GLsizeiptr, GLuint};
 use glfw::Key::Escape;
 use glfw::{Context, Window};
+use std::ffi::CString;
+use std::mem::size_of;
 use std::process::exit;
 use std::ptr::null;
 
 fn main() {
-    let mut glfw1 = glfw::init(glfw::fail_on_errors).unwrap();
-    glfw1.window_hint(glfw::WindowHint::ContextVersion(4, 6));
-    glfw1.window_hint(glfw::WindowHint::OpenGlProfile(
+    let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
         glfw::OpenGlProfileHint::Core,
     ));
 
-    let mut window = glfw1
+    let (mut window, _) = glfw
         .create_window(800, 600, "Hello World", glfw::WindowMode::Windowed)
-        .unwrap()
-        .0;
-    glfw1.make_context_current(Some(&window));
+        .unwrap();
+    glfw.make_context_current(Some(&window));
 
-    gl::load(|e| glfw1.get_proc_address_raw(e) as *const std::os::raw::c_void);
+    gl::load(|e| glfw.get_proc_address_raw(e) as *const _);
+
     unsafe {
         gl::Viewport(0, 0, 800, 600);
     }
@@ -28,112 +28,101 @@ fn main() {
     window.set_framebuffer_size_callback(framebuffer_size_callback);
 
     let shape: [[f32; 3]; 3] = [
-        [-0.5f32, -0.5f32, 0.0f32],
-        [0.5f32, -0.5f32, 0.0f32],
-        [0.0f32, 0.5f32, 0.0f32],
+        [-0.5, -0.5, 0.0],
+        [0.5, -0.5, 0.0],
+        [0.0, 0.5, 0.0],
     ];
 
-    let mut VBO: GLuint = 0;
+    let mut vao: GLuint = 0;
+    let mut vbo: GLuint = 0;
     unsafe {
-        gl::GenBuffers(1, &mut VBO);
-        gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
-    }
+        gl::GenVertexArrays(1, &mut vao);
+        gl::GenBuffers(1, &mut vbo);
 
-    let vertex_shader_source: &str = r##"#version 460 core
-layout (location = 0) in vec3 aPos;
+        gl::BindVertexArray(vao);
 
-void main()
-{
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-}"##;
-
-    let mut vertex_shader: u32 = 0;
-    unsafe {
-        vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
-        let sources = [vertex_shader_source.as_ptr() as *const i8];
-        gl::ShaderSource(vertex_shader, 1, sources.as_ptr(), null());
-        gl::CompileShader(vertex_shader);
-    };
-
-    let fragment_shader_source: &str = r##"#version 460 core
-out vec4 FragColor;
-
-void main()
-{
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-} "##;
-
-    let mut fragment_shader: u32 = 0;
-    unsafe {
-        fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
-        let sources = [fragment_shader_source.as_ptr() as *const i8];
-        gl::ShaderSource(fragment_shader, 1, sources.as_ptr(), null());
-        gl::CompileShader(fragment_shader);
-    };
-
-    let mut shader_program: u32 = 0;
-    unsafe {
-        shader_program = gl::CreateProgram();
-        gl::AttachShader(shader_program, vertex_shader);
-        gl::AttachShader(shader_program, fragment_shader);
-        gl::LinkProgram(shader_program);
-        gl::UseProgram(shader_program);
-        gl::DeleteShader(vertex_shader);
-        gl::DeleteShader(fragment_shader);
-
-        gl::VertexAttribPointer(
-            0,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            (3 * size_of::<f32>()) as GLsizei,
-            0 as *mut std::os::raw::c_void,
-        );
-        gl::EnableVertexAttribArray(0);
-    }
-
-    let mut VAO: u32 = 0;
-    unsafe {
-        gl::GenVertexArrays(1, &mut VAO);
-        gl::BindVertexArray(VAO);
-        gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
         gl::BufferData(
             gl::ARRAY_BUFFER,
-            size_of::<[[f32; 3]; 3]>() as GLsizeiptr,
-            shape.as_ptr() as *const std::os::raw::c_void,
+            (shape.len() * size_of::<[f32; 3]>()) as GLsizeiptr,
+            shape.as_ptr() as *const _,
             gl::STATIC_DRAW,
         );
+
         gl::VertexAttribPointer(
             0,
             3,
             gl::FLOAT,
             gl::FALSE,
             (3 * size_of::<f32>()) as GLsizei,
-            0 as *mut std::os::raw::c_void,
+            null(),
         );
         gl::EnableVertexAttribArray(0);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
     }
+
+    let vertex_shader_source = CString::new(
+        "#version 460 core\n\
+        layout (location = 0) in vec3 aPos;\n\
+        void main() {\n\
+            gl_Position = vec4(aPos, 1.0);\n\
+        }",
+    )
+        .unwrap();
+
+    let fragment_shader_source = CString::new(
+        "#version 460 core\n\
+        out vec4 FragColor;\n\
+        void main() {\n\
+            FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n\
+        }",
+    )
+        .unwrap();
+
+    let vertex_shader = unsafe {
+        let shader = gl::CreateShader(gl::VERTEX_SHADER);
+        gl::ShaderSource(shader, 1, &vertex_shader_source.as_ptr(), null());
+        gl::CompileShader(shader);
+        shader
+    };
+
+    let fragment_shader = unsafe {
+        let shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        gl::ShaderSource(shader, 1, &fragment_shader_source.as_ptr(), null());
+        gl::CompileShader(shader);
+        shader
+    };
+
+    let shader_program = unsafe {
+        let program = gl::CreateProgram();
+        gl::AttachShader(program, vertex_shader);
+        gl::AttachShader(program, fragment_shader);
+        gl::LinkProgram(program);
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+        program
+    };
 
     while !window.should_close() {
         process_input(&window);
 
         unsafe {
-            gl::UseProgram(shader_program);
-            gl::ClearColor(0.2f32, 0.3f32, 0.3f32, 1.0f32);
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
-            gl::BindVertexArray(VAO);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
 
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
         }
 
         window.swap_buffers();
-        glfw1.poll_events();
+        glfw.poll_events();
     }
-
-    println!("Hello, world!");
 }
 
-fn framebuffer_size_callback(window: &mut Window, width: i32, height: i32) {
+fn framebuffer_size_callback(_window: &mut Window, width: i32, height: i32) {
     unsafe {
         gl::Viewport(0, 0, width as GLsizei, height as GLsizei);
     }
